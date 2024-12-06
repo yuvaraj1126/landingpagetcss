5a8c4115751a20cc03d07a435650c851cf697c67
yuvaraj1126 <yuvarajgowtham30@gmail.com>
2024-12-06 21:42:39 +0530

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {fontFamily: {
+      righteous: ['Righteous', 'cursive'], // Add the font
+    },},
+  },
+  plugins: [],
+}
+
